c509e5880f31434e7dcf9ca3fa17b761b55d6eb7
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-08-28 14:54:59 +0200

    Created

---
+++ src/paint/frontend/components/PaintPanel.java
@@ -0,0 +1,10 @@
+package paint.frontend.components;
+
+import javax.swing.JPanel;
+
+public class PaintPanel extends JPanel {
+
+	
+	private static final long serialVersionUID = 4414133012207354491L;
+
+}
---
+++ src/paint/frontend/mainFrame/PaintGUI.java
@@ -0,0 +1,24 @@
+package paint.frontend.mainFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class PaintGUI extends JFrame implements ActionListener {
+
+	
+	private static final long serialVersionUID = 5138582323739354636L;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
