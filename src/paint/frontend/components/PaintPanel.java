c8644322e60572eae16624f431effbca42680f7f
FrancescoGhinamo <44404163+FrancescoGhinamo@users.noreply.github.com>
2019-08-28 15:08:30 +0200

    Basic functions

--- src/paint/frontend/components/PaintPanel.java
+++ src/paint/frontend/components/PaintPanel.java
@@ -1,10 +1,57 @@
 package paint.frontend.components;
 
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
-public class PaintPanel extends JPanel {
+public class PaintPanel extends JPanel implements MouseMotionListener {
 
 	
 	private static final long serialVersionUID = 4414133012207354491L;
+	
+	private int thickness;
+	
+	private ArrayList<Point> points;
+	
+	public PaintPanel() {
+		this.addMouseMotionListener(this);
+		points = new ArrayList<Point>();
+		this.thickness = 10;
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		for(Point p: points) {
+			g.fillOval(p.x, p.y, thickness, thickness);
+		}
+		
+	}
 
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		points.add(e.getPoint());
+		repaint();
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getThickness() {
+		return thickness;
+	}
+
+	public void setThickness(int thickness) {
+		this.thickness = thickness;
+	}
+
+	
+	
 }
--- src/paint/frontend/mainFrame/PaintGUI.java
+++ src/paint/frontend/mainFrame/PaintGUI.java
@@ -1,14 +1,34 @@
 package paint.frontend.mainFrame;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 
+import paint.frontend.components.PaintPanel;
+
 public class PaintGUI extends JFrame implements ActionListener {
 
 	
 	private static final long serialVersionUID = 5138582323739354636L;
+	
+	private PaintPanel paintPanel;
+	
+	public PaintGUI() {
+		super("Paint");
+		setExtendedState(MAXIMIZED_BOTH);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		initComponents();
+	}
+
+	private void initComponents() {
+		setLayout(new BorderLayout());
+		
+		paintPanel = new PaintPanel();
+		this.add(paintPanel, BorderLayout.CENTER);
+		
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -17,7 +37,7 @@ public class PaintGUI extends JFrame implements ActionListener {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new PaintGUI().setVisible(true);
 
 	}
 
